60416176b46012def3121adb19425216b035a942
Nhlakanipho Mathibela <94120861+NiphoMathibela@users.noreply.github.com>
2025-09-23 14:27:19 +0200

    Add BSON attributes to Users model for MongoDB serialization

--- ApplicationInterface/Models/Users.cs
+++ ApplicationInterface/Models/Users.cs
@@ -1,6 +1,15 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 public class Users
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public int Id { get; set; }
+
+    [BsonElement("Name")]
     public string Name { get; set; }
+
+    [BsonElement("Email")]
     public string Email { get; set; }
 }
