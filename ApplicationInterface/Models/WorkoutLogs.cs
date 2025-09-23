972737a112560c50cb2633f453974effe6c9be6a
Nhlakanipho Mathibela <94120861+NiphoMathibela@users.noreply.github.com>
2025-09-23 14:33:14 +0200

    Add BSON attributes to WorkoutLogs model for MongoDB serialization

--- ApplicationInterface/Models/WorkoutLogs.cs
+++ ApplicationInterface/Models/WorkoutLogs.cs
@@ -1,8 +1,21 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+
 public class WorkoutLogs
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public int Id { get; set; }
+
+    [BsonElement("userId")]
     public string UserId { get; set; }
+
+    [BsonElement("workoutId")]
     public int WorkoutId { get; set; }
+
+    [BsonElement("date")]
     public DateTime Date { get; set; }
 
 }
