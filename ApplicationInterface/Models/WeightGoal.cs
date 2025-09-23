654e1794d1fab160b7d605c3353e2bc493e26a93
Nhlakanipho Mathibela <94120861+NiphoMathibela@users.noreply.github.com>
2025-09-23 14:34:55 +0200

    Add BSON attributes to WeightGoal model for MongoDB serialization

--- ApplicationInterface/Models/WeightGoal.cs
+++ ApplicationInterface/Models/WeightGoal.cs
@@ -1,8 +1,21 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 public class WeightGoal
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public int Id { get; set; }
+
+    [BsonElement("userId")]
     public string UserId { get; set; }
+
+    [BsonElement("targetWeight")]
     public float TargetWeight { get; set; }
+
+    [BsonElement("currentWeight")] 
     public float CurrentWeight { get; set; }
+
+    [BsonElement("targetDate")]
     public DateTime TargetDate { get; set; }
 }
